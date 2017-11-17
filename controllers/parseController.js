51ee0b294deca042805c2136d1ba0c080119b016
Y.Kalishchuk <yura.kalishchuk@gmail.com>
2017-11-17 17:02:59 +0200

    remove debug script

--- controllers/parseController.js
+++ controllers/parseController.js
@@ -67,14 +67,7 @@ async function load(url = '/catalog') {
 
                         if (categories.length > 0) {
 
-                            if(categories.length == 1)
-                                q.push(categories);
-                            else if(categories[0].depth_level != 5)
-                                q.push(categories[1]);
-                            else
-                                q.push(categories);
-
-                            //q.push(categories);//add next categories to query
+                            q.push(categories);//add next categories to query
                             log.step(categories.length);
                             callback(); //call callback in the end
                         }
